3c8e752d62d007e343be29f00a0d5b1138610ab1
Roberto Lentini <rob.len001@gmail.com>
2021-07-22 10:57:45 -0400

    documentation

--- R/installPackages.R
+++ R/installPackages.R
@@ -7,6 +7,11 @@
 #'
 installPackages <- function(){
 
+  if (Sys.which(('pdflatex') == "")){
+    install.packages('tinytex')
+    tinytex::install_tinytex()
+  }
+
   utils::install.packages(c("shiny","shinyFiles", "shinyjs", "shinycssloaders", "rmarkdown", "tidytext",
                      "tidyverse", "pluralize", "data.table", "dplyr", "tidyr", "tibble", "utils", "fs", "readr"))
 
